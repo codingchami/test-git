9b98d95bb61dc8a3592edb065d76f48df6cd665b
codingchami <cschamithsandeepa@gmail.com>
2024-07-03 15:40:46 +0530

    added a for loop for print "Hello"

--- src/Example.java
+++ src/Example.java
@@ -2,6 +2,10 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("Hello Java");
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+
+        }
 
     }
 }
